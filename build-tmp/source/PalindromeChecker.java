fb40be748a7bb6bbb48c2cd9b3d71b52170f735e
luchen825 <lucychen28@gmail.com>
2015-01-13 10:43:09 -0800

    finished

--- build-tmp/source/PalindromeChecker.java
+++ build-tmp/source/PalindromeChecker.java
@@ -22,18 +22,38 @@ public void setup()
   {
     if(palindrome(lines[i])==true)
     {
-      println(lines[i] + " IS a palidrome.");
+      println(lines[i] + " IS a palindrome.");
     }
     else
     {
-      println(lines[i] + " is NOT a palidrome.");
+      println(lines[i] + " is NOT a palindrome.");
     }
   }
 }
 public boolean palindrome(String word)
 {
-  //your code here
-  return false;
+  String temp = "";
+  word = word.toLowerCase();
+  for(int i=0; i < word.length(); i++)
+  {
+    if(Character.isLetter(word.charAt(i)) == true)
+    {
+      temp = temp + word.charAt(i);
+    }
+  }
+  int j = temp.length() - 1;
+  for(int i = 0; i < temp.length()/2; i++)
+  {
+    if(temp.charAt(i) != temp.charAt(j))
+    {
+      return false;
+    }
+    else
+    {
+      j--;
+    }
+  }
+  return true;
 }
 
   static public void main(String[] passedArgs) {
